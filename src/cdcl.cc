b596a54e1b80f6435c75100800689634efdc90c5
Aaron Windsor <aaron.windsor@gmail.com>
2019-04-04 06:34:17 -0400

    toasty

--- src/cdcl.cc
+++ src/cdcl.cc
@@ -222,8 +222,9 @@ Cnf parse(const char* filename) {
 // Returns true exactly when a satisfying assignment exists for c.
 bool solve(Cnf* c) {
     lit_t d = 0;
-    do {
+    while (true) {
         // (C2)
+        LOG(4) << "C2";
         while (c->f == c->g) {
             LOG(4) << "C5";
             // C5
@@ -321,166 +322,167 @@ bool solve(Cnf* c) {
         if (!found_conflict) {
             LOG(3) << "Emptying " << -l << "'s watch list";
             c->watch[-l] = clause_nil;
-        } else {
-            // C7
-            LOG(3) << "Found a conflict with d = " << d;
-            if (d == 0) return false;
+            continue;
+        }
 
-            // Not mentioned in Knuth's description, but we need to make sure
-            // that the rightmost literal on the trail is the first literal
-            // in the clause here.
-            // TODO: i think it's okay to blindly swap out the watchlist here
-            // without regard to splicing the singly linked list since all
-            // literals in the clause are false?
-            size_t rl = c->f;
-            size_t cs = static_cast<size_t>(c->clauses[w-1]);
-            for (bool done = false; !done; --rl) {
-                for (size_t j = 0; j < cs; ++j) {
-                    if (abs(c->trail[rl]) == abs(c->clauses[w+j])) {
-                        done = true;
-                        std::swap(c->clauses[w], c->clauses[w+j]);
-                        break;
-                    }
+        // C7
+        LOG(3) << "Found a conflict with d = " << d;
+        if (d == 0) return false;
+        
+        // Not mentioned in Knuth's description, but we need to make sure
+        // that the rightmost literal on the trail is the first literal
+        // in the clause here.
+        // TODO: i think it's okay to blindly swap out the watchlist here
+        // without regard to splicing the singly linked list since all
+        // literals in the clause are false?
+        size_t rl = c->f;
+        size_t cs = static_cast<size_t>(c->clauses[w-1]);
+        for (bool done = false; !done; --rl) {
+            for (size_t j = 0; j < cs; ++j) {
+                if (abs(c->trail[rl]) == abs(c->clauses[w+j])) {
+                    done = true;
+                    std::swap(c->clauses[w], c->clauses[w+j]);
+                    break;
                 }
             }
+        }
 
-            lit_t dp = 0;
-            lit_t q = 0;
-            lit_t r = 0;
-            c->epoch++;
-            LOG(3) << "Bumping epoch to " << c->epoch << " at "
-                   << c->print_clause(w);
-            LOG(3) << "Trail is " << c->print_trail();
-            c->stamp[abs(c->clauses[w])] = c->epoch;
-            c->heap.bump(abs(c->clauses[w]));
-            lit_t t = -1;
-            for(size_t j = 1; j < static_cast<size_t>(c->clauses[w-1]); ++j) {
-                lit_t m = c->clauses[w+j];
-                if (c->tloc[abs(m)] >= t) t = c->tloc[abs(m)];
-                // TODO: technically don't need this next line, but it's part of
-                // the blit subroutine
-                if (c->stamp[abs(m)] == c->epoch) continue;
-                c->stamp[abs(m)] = c->epoch;
-                lit_t p = c->lev[abs(m)];
-                if (p > 0) c->heap.bump(abs(m));
-                if (p == d) {
-                    LOG(3) << m << " is at level " << d;
-                    q++;
-                } else {
-                    LOG(3) << "Adding " << -m << " (level " << p << ") to learned clause.";
-                    c->b[r] = -m;
-                    r++;
-                    dp = std::max(dp, p);
-                }
+        lit_t dp = 0;
+        lit_t q = 0;
+        lit_t r = 0;
+        c->epoch++;
+        LOG(3) << "Bumping epoch to " << c->epoch << " at "
+               << c->print_clause(w);
+        LOG(3) << "Trail is " << c->print_trail();
+        c->stamp[abs(c->clauses[w])] = c->epoch;
+        c->heap.bump(abs(c->clauses[w]));
+        lit_t t = -1;
+        for(size_t j = 1; j < static_cast<size_t>(c->clauses[w-1]); ++j) {
+            lit_t m = c->clauses[w+j];
+            if (c->tloc[abs(m)] >= t) t = c->tloc[abs(m)];
+            // TODO: technically don't need this next line, but it's part of
+            // the blit subroutine
+            if (c->stamp[abs(m)] == c->epoch) continue;
+            c->stamp[abs(m)] = c->epoch;
+            lit_t p = c->lev[abs(m)];
+            if (p > 0) c->heap.bump(abs(m));
+            if (p == d) {
+                LOG(3) << m << " is at level " << d;
+                q++;
+            } else {
+                LOG(3) << "Adding " << -m << " (level " << p << ") to learned clause.";
+                c->b[r] = -m;
+                r++;
+                dp = std::max(dp, p);
             }
-
-            // TODO: knuth says q > 0?
-            while (q > 1) {
-                LOG(3) << "q=" << q << ",t=" << t;
-                lit_t l = c->trail[t];
-                LOG(3) << "Up trail, q=" << q << ", t=" << t << ", l=" << l
-                       << "L_t = " << c->trail[t];
-                t--;
-                LOG(3) << "New L_t = " << c->trail[t];
-                if (c->stamp[abs(l)] == c->epoch) {
-                    LOG(3) << "Stamped this epoch: " << l;
-                    q--;
-                    if (c->reason[l] != clause_nil) {
-                        clause_t rc = c->reason[l];
-                        if (c->clauses[rc] != l) {
-                            // TODO: don't swap here (or similar swap above) 
-                            std::swap(c->clauses[rc], c->clauses[rc+1]);
-                            std::swap(c->clauses[rc-2], c->clauses[rc-3]);
-                        }                        
-                        LOG(3) << "Reason for " << l << ": " << c->print_clause(rc);
-                        for (size_t j = 1; j < static_cast<size_t>(c->clauses[rc-1]); ++j) {
-                            lit_t m = c->clauses[rc+j];
-                            LOG(3) << "considering " << abs(m);
-                            if (c->stamp[abs(m)] == c->epoch) continue;
-                            c->stamp[abs(m)] = c->epoch;
-                            lit_t p = c->lev[abs(m)];
-                            if (p > 0) c->heap.bump(abs(m));
-                            if (p == d) {
-                                q++;
-                            } else {
-                                LOG(3) << "Adding " << -m << " to learned clause.";
-                                c->b[r] = -m;
-                                r++;
-                                dp = std::max(dp, p);
-                            }
+        }
+        
+        // TODO: knuth says q > 0?
+        while (q > 1) {
+            LOG(3) << "q=" << q << ",t=" << t;
+            lit_t l = c->trail[t];
+            LOG(3) << "Up trail, q=" << q << ", t=" << t << ", l=" << l
+                   << "L_t = " << c->trail[t];
+            t--;
+            LOG(3) << "New L_t = " << c->trail[t];
+            if (c->stamp[abs(l)] == c->epoch) {
+                LOG(3) << "Stamped this epoch: " << l;
+                q--;
+                if (c->reason[l] != clause_nil) {
+                    clause_t rc = c->reason[l];
+                    if (c->clauses[rc] != l) {
+                        // TODO: don't swap here (or similar swap above) 
+                        std::swap(c->clauses[rc], c->clauses[rc+1]);
+                        std::swap(c->clauses[rc-2], c->clauses[rc-3]);
+                    }                        
+                    LOG(3) << "Reason for " << l << ": " << c->print_clause(rc);
+                    for (size_t j = 1; j < static_cast<size_t>(c->clauses[rc-1]); ++j) {
+                        lit_t m = c->clauses[rc+j];
+                        LOG(3) << "considering " << abs(m);
+                        if (c->stamp[abs(m)] == c->epoch) continue;
+                        c->stamp[abs(m)] = c->epoch;
+                        lit_t p = c->lev[abs(m)];
+                        if (p > 0) c->heap.bump(abs(m));
+                        if (p == d) {
+                            q++;
+                        } else {
+                            LOG(3) << "Adding " << -m << " to learned clause.";
+                            c->b[r] = -m;
+                            r++;
+                            dp = std::max(dp, p);
                         }
                     }
                 }
             }
-
-            lit_t lp = c->trail[t];
-            LOG(4) << "lp = " << lp;
-            // TODO: knuth says "while S(|l'|) != s set t = t-1 and l' <- L_t"
-            // in answer to #263, but in his impl he has "o,l=trail[tl--];"
-            while (c->stamp[abs(lp)] != c->epoch) { t--; lp = c->trail[t]; }
-
-            LOG(4) << "stopping C7 with l'=" << lp;
-
-            // Debugging for learned clause:
-            std::ostringstream oss;
-            oss << -lp << " ";
-            for(int i = 0; i < r; i++) {
-                oss << -c->b[i] << " ";
-            }
-            LOG(3) << "[*] dp = " << dp << ", learned clause is: " << oss.str();
-            
-            // C8: backjump
-            while (c->f > c->di[dp+1]) {
-                c->f--;
-                lit_t l = c->trail[c->f];
-                LOG(3) << "Backjumping: " << l;
-                lit_t k = abs(l);
-                c->oval[k] = c->val[k];
-                c->val[k] = UNSET;
-                c->reason[k] = clause_nil;
-                c->heap.insert(k);
-            }
-            c->g = c->f;
-            d = dp;
-            LOG(3) << "After backjump, trail is " << c->print_trail();
-            
-            // C9: learn
-            //TODO: Knuth has "if d > 0 do step C9". what is else clause?
-            // assuming it's "return false" here.
-            if (d == 0) return false;
-
-            c->clauses.push_back(clause_nil); // watch list for l1
-            c->clauses.push_back(c->watch[-lp]); // watch list for l0
-            c->clauses.push_back(r+1); // size
-            LOG(3) << "adding a clause of size " << r+1;
-            clause_t lc = c->clauses.size();
-            c->clauses.push_back(-lp);
-            c->watch[-lp] = lc;
-            bool found_watch = false;
-            for (lit_t j = 0; j < r; ++j) {
-                if (found_watch || c->lev[abs(c->b[j])] < dp) {
-                    c->clauses.push_back(-c->b[j]);
-                } else {
-                    c->clauses[lc+1] = -c->b[j];
-                    c->clauses[lc-3] = c->watch[-c->b[j]];
-                    c->watch[-c->b[j]] = lc;
-                    found_watch = true;
-                }
-            }
-            LOG(3) << "*** Successfully added clause " << c->print_clause(lc);
-
-            // TODO: Knuth says "lp" here, but I think it's "-lp"?
-            c->trail[c->f] = -lp;
-            c->val[abs(lp)] = -lp < 0 ? FALSE : TRUE;
-            c->lev[abs(lp)] = d;
-            c->tloc[abs(lp)] = c->f;
-            c->reason[abs(lp)] = lc;
-            c->f++;
-            // TODO: DEL = DEL / rho;
-
-            LOG(3) << "After clause install, trail is " << c->print_trail();
         }
-    } while (c->f < static_cast<size_t>(c->nvars));
+
+        lit_t lp = c->trail[t];
+        LOG(4) << "lp = " << lp;
+        // TODO: knuth says "while S(|l'|) != s set t = t-1 and l' <- L_t"
+        // in answer to #263, but in his impl he has "o,l=trail[tl--];"
+        while (c->stamp[abs(lp)] != c->epoch) { t--; lp = c->trail[t]; }
+        
+        LOG(4) << "stopping C7 with l'=" << lp;
+        
+        // Debugging for learned clause:
+        std::ostringstream oss;
+        oss << -lp << " ";
+        for(int i = 0; i < r; i++) {
+            oss << -c->b[i] << " ";
+        }
+        LOG(3) << "[*] dp = " << dp << ", learned clause is: " << oss.str();
+        
+        // C8: backjump
+        while (c->f > c->di[dp+1]) {
+            c->f--;
+            lit_t l = c->trail[c->f];
+            LOG(3) << "Backjumping: " << l;
+            lit_t k = abs(l);
+            c->oval[k] = c->val[k];
+            c->val[k] = UNSET;
+            c->reason[k] = clause_nil;
+            c->heap.insert(k);
+        }
+        c->g = c->f;
+        d = dp;
+        LOG(3) << "After backjump, trail is " << c->print_trail();
+        
+        // C9: learn
+        //TODO: Knuth has "if d > 0 do step C9". what is else clause?
+        // assuming it's "return false" here.
+        if (d == 0) return false;
+        
+        c->clauses.push_back(clause_nil); // watch list for l1
+        c->clauses.push_back(c->watch[-lp]); // watch list for l0
+        c->clauses.push_back(r+1); // size
+        LOG(3) << "adding a clause of size " << r+1;
+        clause_t lc = c->clauses.size();
+        c->clauses.push_back(-lp);
+        c->watch[-lp] = lc;
+        bool found_watch = false;
+        for (lit_t j = 0; j < r; ++j) {
+            if (found_watch || c->lev[abs(c->b[j])] < dp) {
+                c->clauses.push_back(-c->b[j]);
+            } else {
+                c->clauses[lc+1] = -c->b[j];
+                c->clauses[lc-3] = c->watch[-c->b[j]];
+                c->watch[-c->b[j]] = lc;
+                found_watch = true;
+            }
+        }
+        LOG(3) << "*** Successfully added clause " << c->print_clause(lc);
+        
+        // TODO: Knuth says "lp" here, but I think it's "-lp"?
+        c->trail[c->f] = -lp;
+        c->val[abs(lp)] = -lp < 0 ? FALSE : TRUE;
+        c->lev[abs(lp)] = d;
+        c->tloc[abs(lp)] = c->f;
+        c->reason[abs(lp)] = lc;
+        c->f++;
+        // TODO: DEL = DEL / rho;
+        
+        LOG(3) << "After clause install, trail is " << c->print_trail();
+    }
 
     return true;
 }
